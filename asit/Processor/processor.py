d5ee4c1df37c1e67656c7f92a28fec2650a9de76
Ali Gholami <aligholami7596@gmail.com>
2018-04-01 00:15:58 +0430

    Get number of CPU Cores

---
+++ asit/Processor/processor.py
@@ -0,0 +1,41 @@
+# ========================================
+# [] File Name : processor.py
+#
+# [] Creation Date : March 2018
+#
+# [] Created By : Ali Gholami (aligholami7596@gmail.com)
+# ========================================
+"""
+    Advanced System Information Toolbox Python Library.
+"""
+import platform
+import psutil
+
+class Processor:
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def get_name(self):
+        """
+        :return: Processor's name
+        """
+        return platform.processor()
+
+    @staticmethod
+    def get_code_name(self):
+        """
+        :return: Processor's code name
+        """
+        return platform.processor()
+
+    @staticmethod
+    def get_num_cores(self):
+        """
+        :return: Number of processor cores
+        """
+        return psutil.cpu_count()
+
+
+
